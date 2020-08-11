eed16c97d6d22132d38161f51feea2f226b31fe4
Sophie <sophiebrown438@gmail.com>
2020-08-11 07:58:38 +0100

    Alert when order will be too long

--- __tests__/shack.test.js
+++ __tests__/shack.test.js
@@ -3,15 +3,19 @@ let shack = new Shack
 
 
 describe('#Shack', () => {
-  test('it should return how long to make order', () => {
-    expect(shack.makeSandwich(5)).toBe('Your order will be 7.5 minutes')
+  test('it should alert when an order will be too long', () => {
+    expect(shack.makeSandwich(5)).toBe('Your order will be more than 5 minutes')
   })
 
-  test('it should return how long to make order', () => {
-    expect(shack.makeSandwich(15)).toBe('Your order will be 22.5 minutes')
+  test('it should alert when an order will be too long', () => {
+    expect(shack.makeSandwich(15)).toBe('Your order will be more than 5 minutes')
   })
 
   test('it should return how long to make order', () => {
     expect(shack.makeSandwich(1)).toBe('Your order will be 1.5 minutes')
+  })
+
+  test('it should return how long to make order', () => {
+    expect(shack.makeSandwich(2)).toBe('Your order will be 3 minutes')
   })
 })
--- src/shack.js
+++ src/shack.js
@@ -1,8 +1,16 @@
 class Shack {
+  constructor(){
+    this.WAITING_TIME = 5
+  }
 
   makeSandwich(order) {
     let orderTime = (order * 1.5)
+    if (orderTime >= this.WAITING_TIME) {
+      return 'Your order will be more than 5 minutes'
+    }
+    else {
     return (`Your order will be ${orderTime} minutes`) 
+    }
   }
 }
 
