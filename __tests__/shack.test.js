701b1c99498168a9a5d4db8e6bbbbe186f1cefb3
Sophie <sophiebrown438@gmail.com>
2020-08-11 19:29:55 +0100

    Pass test for formatting orders

--- __tests__/shack.test.js
+++ __tests__/shack.test.js
@@ -18,4 +18,12 @@ describe('#Shack', () => {
   test('it should return how long to make order', () => {
     expect(shack.makeSandwich(2)).toBe('Your order will be 3 minutes')
   })
+
+  test('it should return how long to make order', () => {
+    expect(shack.sequenceOrder(1,0)).toEqual('1. 1 sandwich orders placed, start making sandwich 1\n2. serve sandwich 1')
+  })
+
+  // test('it should return how long to make order', () => {
+  //   expect(shack.sequenceOrder(2,0)).toEqual('Your order will be 3 minutes')
+  // })
 })
--- src/shack.js
+++ src/shack.js
@@ -8,7 +8,7 @@ class Shack {
   makeSandwich(order) {
     let orderTime = (order * 1.5)
     if (orderTime >= this.WAITING_TIME) {
-      return('Your order has been rejected as it will be more than 5 minutes')
+      return 'Your order has been rejected as it will be more than 5 minutes' 
     }
     else if (this.inventory >= order ){
       this.totalOrders.push(order)
@@ -17,9 +17,17 @@ class Shack {
     }
   }
 
-  sequenceOrder(){
-
-  }
+  sequenceOrder(sandwiches, jacketPotato){
+    let orderTime = (sandwiches * 1.5)
+    // if (orderTime < this.WAITING_TIME) {
+      let i = 0
+      while (sandwiches >= i){
+        i++
+        return `${i}. ${sandwiches} sandwich orders placed, start making sandwich ${i}\n${i+1}. serve sandwich ${i}`
+    }
+  // }
+}
+  
 }
 
 module.exports = Shack
