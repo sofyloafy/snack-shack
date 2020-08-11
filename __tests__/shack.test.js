5375c2c22c9da55c0fcd32300ea4a96b1c3f053a
Sophie <sophiebrown438@gmail.com>
2020-08-11 07:44:11 +0100

    Amend return val w string

--- __tests__/shack.test.js
+++ __tests__/shack.test.js
@@ -4,10 +4,10 @@ let shack = new Shack
 
 describe('#Shack', () => {
   test('it should return how long to make order', () => {
-    expect(shack.makeSandwich([5])).toBe(7.5)
+    expect(shack.makeSandwich([5])).toBe('Your order will be 7.5 minutes')
   })
 
   test('it should return how long to make order', () => {
-    expect(shack.makeSandwich([5, 10])).toBe(22.5)
+    expect(shack.makeSandwich([5, 10])).toBe('Your order will be 22.5 minutes')
   })
 })
--- src/shack.js
+++ src/shack.js
@@ -3,7 +3,7 @@ class Shack {
   makeSandwich(order) {
     let orderTime = 0
     order.map(e => orderTime += e * 1.5)
-    return orderTime
+    return (`Your order will be ${orderTime} minutes`) 
   }
 }
 
