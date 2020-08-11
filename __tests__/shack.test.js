bd56d31b01959df605c274f1b39e396b3cc91828
Sophie <sophiebrown438@gmail.com>
2020-08-11 07:53:23 +0100

    makeSandwich func test

--- __tests__/shack.test.js
+++ __tests__/shack.test.js
@@ -4,10 +4,14 @@ let shack = new Shack
 
 describe('#Shack', () => {
   test('it should return how long to make order', () => {
-    expect(shack.makeSandwich([5])).toBe('Your order will be 7.5 minutes')
+    expect(shack.makeSandwich(5)).toBe('Your order will be 7.5 minutes')
   })
 
   test('it should return how long to make order', () => {
-    expect(shack.makeSandwich([5, 10])).toBe('Your order will be 22.5 minutes')
+    expect(shack.makeSandwich(15)).toBe('Your order will be 22.5 minutes')
+  })
+
+  test('it should return how long to make order', () => {
+    expect(shack.makeSandwich(1)).toBe('Your order will be 1.5 minutes')
   })
 })
--- src/shack.js
+++ src/shack.js
@@ -1,8 +1,7 @@
 class Shack {
 
   makeSandwich(order) {
-    let orderTime = 0
-    order.map(e => orderTime += e * 1.5)
+    let orderTime = (order * 1.5)
     return (`Your order will be ${orderTime} minutes`) 
   }
 }
